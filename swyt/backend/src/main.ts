ec9f3779a1f43c22ca8e0b5342993b8647494081
imad <code.isaad@gmail.com>
2024-01-23 10:25:46 +0400

    INITIATE: initiated the backend with postgres, and prisma in integration, all containarized

---
+++ swyt/backend/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+async function bootstrap() {
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  // Enable CORS
+  app.enableCors();
+
+  await app.listen(3000);
+}
+bootstrap();
--- swyt/frontend/src/App.vue
+++ swyt/frontend/src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <v-card class="w-50">
-    hello
-  </v-card>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+
+
+
+onMounted(async () => {
+  const response = await axios.get('http://10.63.175.120:3000/url')
+  console.log(response.data)
+  // console.log('API URL:', process.env.IP);
+})
 </script>
 
 <style>
